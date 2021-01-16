77e648f5e0fdc77d74a4d189a712a91fade69188
AJAY KUMAR <48375037+AJAYKR00KJ@users.noreply.github.com>
2021-01-16 06:35:45 +0530

    update profile.php

--- Front_end/profile.php
+++ Front_end/profile.php
@@ -1,21 +1,4 @@
-<?php 
-  session_start(); 
-
-  if (!isset($_SESSION['email'])) {
-  	$_SESSION['msg'] = "You must log in first";
-  	header('location: login.php');
-  }
-  if (isset($_GET['logout'])) {
-  	session_destroy();
-  	unset($_SESSION['email']);
-  	header("location: landing.html");
-  }
-
-
-  
-
-
-?>
+ 
 
 <!-- --------------------------------------------------------------------------
   
